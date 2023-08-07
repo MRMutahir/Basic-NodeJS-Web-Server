8ddcc3f04ea5d83303d772fea1eef10883e0dc2e
Mhummad Mutahir <121317971+MRMutahir@users.noreply.github.com>
2023-08-07 14:01:40 +0500

    git create server with three example

--- index.js
+++ index.js
@@ -1,21 +1,98 @@
-const { error } = require('console');
-const http = require('http');
-const url = require('url');
-let server =  http.createServer((req,res)=>{
-    if (req.url === '/') {
-        res.end('server Run!');
-    }else if (req.url === '/about') {
-        res.end('server  Run About ! ');
-        
-    }else if(req.url === '/contact'){
-        res.end('server  Run contact ! ');
+console.log('SALAM');
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> i cover  emit events   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// const eventEmitter = require('events');
+// const mYEmitter = new eventEmitter();
+// mYEmitter.on('mr',()=>{
+//     console.log('first events');
+// });
+// mYEmitter.on('m',()=>{
+//     console.log('second events');
+// });
+// mYEmitter.on('num',number=>{
+//     console.log(number);
+// })
+// mYEmitter.emit('mr');
+// mYEmitter.emit('m');
+// mYEmitter.emit("num",10+10);
 
-    }else{
-        // console.log(error,'server not run ?');
-        res.end('incorect url')
-    }
-});
-let port = 8000
-server.listen(port,'127.0.0.1',()=>{
-console.log(`listen to server ${port}`);
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> create server  first Example <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// const server = require('http').createServer()
+// const fs =  require('fs');
+const url = require('url')
+// server.on('request',(req,res)=>{
+//     if(req == '/favicon.ico ') return false
+//     fs.readFile("./text3",(error,data)=>{
+//         res.end(data)
+//     })
+//     // fs.appendFile(`text3`,`${req.url}  ek new req i hen`,(error,data)=>{
+//     //     // res.end('well come home page')
+//     //     res.end(data)
+//     // })
+// });
+// let port = 8000
+// server.listen(8000,'127.0.0.1',()=>{
+//     console.log(('server started'));
+// })
+// server.on('request',(req,res)=>{
+//     fs.readFile("./text3",(error,data)=>{
+// res.end(data)
+//     })
+// })
+// server.listen(8000,'127.0.0.1',()=>{
+//     console.log('server listening');
+// })
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> create server  second Example <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+//  server.on('request',(req,res)=>{
+//     //     fs.readFile("./text3",(error,data)=>{
+//     // res.end(data)
+//     //     })
+//     const reader  = fs.createReadStream('./tex3');
+//    reader.on('data',chunk =>{
+//     res.write(chunk);
+// })
+// reader.on('end',()=>{
+//     res.end()
+// })
+// // reader.on('error',()=>{
+// //     res.statusCode = 404
+// //     res.end('files not found')
+// // })  
+//     })
+//     server.listen(8000,'127.0.0.1',()=>{
+//         console.log('server listening');
+//     })
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> create server  three  Example <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+// server.on('request',(res,req)=>{
+// })
+// const reader  = fs.createReadStream('./tex3');
+// reader.pipe(res) 
+
+// server.listen(8000,'120.0.0.1',()=>{
+//     console.log("server on");
+// })
+
+
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer()
+server.on('request',(req,res)=>{
+    const reader = fs.createReadStream('./text3');
+
+    // Set the appropriate headers for the response
+    res.setHeader('Content-Type', 'text/plain');
+
+    // Pipe the contents of the file to the response stream
+    reader.pipe(res);
 })
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log("Server is running at http://127.0.0.1:8000");
+});
